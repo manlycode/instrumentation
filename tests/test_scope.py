bd31c38883288cb937f1ec94e809c71947d004f9
Chris Rittersdorf <chris.rittersdorf@collectiveidea.com>
2023-01-21 23:15:54 -0500

    Add channels coupling tests

--- tests/test_scope.py
+++ tests/test_scope.py
@@ -1,10 +1,11 @@
 from tests import scope
-from instrumentation.siglent.scope import BWLimit
+from instrumentation.siglent.scope import BWLimit, Coupling
+
+channel = scope.channel(1)
+channels = scope.channels([3, 4])
 
 
 def test_channel_bwLimit():
-    channel = scope.channel(1)
-
     channel.bwLimit(BWLimit.BWL_200M)
     assert channel.bwLimit() == "FULL"
 
@@ -13,10 +14,24 @@ def test_channel_bwLimit():
 
 
 def test_channels_bwLimit():
-    channels = scope.channels([1, 2, 3, 4])
-
     channels.bwLimit(BWLimit.BWL_200M)
-    assert channels.bwLimit() == ["FULL", "FULL", "FULL", "FULL"]
+    assert channels.bwLimit() == ["FULL", "FULL"]
 
     channels.bwLimit(BWLimit.BWL_20M)
-    assert channels.bwLimit() == ["20M", "20M", "20M", "20M"]
+    assert channels.bwLimit() == ["20M", "20M"]
+
+
+def test_channel_coupling():
+    channel.coupling(Coupling.AC)
+    assert channel.coupling() == "AC"
+
+    channel.coupling(Coupling.DC)
+    assert channel.coupling() == "DC"
+
+
+def test_channels_coupling():
+    channels.coupling(Coupling.AC)
+    assert channels.coupling() == ["AC", "AC"]
+
+    channels.coupling(Coupling.DC)
+    assert channels.coupling() == ["DC", "DC"]
